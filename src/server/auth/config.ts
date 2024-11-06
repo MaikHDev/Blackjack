5a5891cf79b6c387db6102e965150d548bd617aa
Maik Hendriks <123378700+MaikHDev@users.noreply.github.com>
2024-11-06 23:58:51 +0100

    Google auth, database url

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,8 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import Google from "next-auth/providers/google"
+
 
 import { db } from "@/server/db";
 import {
@@ -39,6 +41,7 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     DiscordProvider,
+    Google
     /**
      * ...add more providers here.
      *
