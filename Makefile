1ccbc90783a6059e15774da52623f964b6c52c35
Rutger Pronk <rutger.pronk11@gmail.com>
2024-11-10 18:02:14 +0100

    Create header

--- Makefile
+++ Makefile
@@ -3,6 +3,20 @@ all: up
 up: ssl
 	docker compose up --watch --build --remove-orphans --force-recreate
 
+up-%: ssl
+	docker compose up --build --detach --remove-orphans --force-recreate $*
+
+restart: up # start already rebuilds and recreates the containers
+
+restart-%:
+	make up-$*
+
+down:
+	docker compose down --remove-orphans
+
+down-%:
+	docker compose down --remove-orphans $*
+
 shell-%:
 	docker compose exec $* bash
 
---
+++ src/app/_components/Header/Header.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const navigation = [
+  { name: "Friends", href: "/friends" },
+  { name: "Quick join", href: "/quick-join" },
+];
+
+export function Header() {
+  return (
+    <div className={"flex w-full justify-center"}>
+      <header className="flex h-20 w-full max-w-7xl items-center justify-between px-20">
+        <Link href={"/"}>
+          <span className={"text-xl"}>21 Hub</span>
+        </Link>
+
+        <nav className={"right-0 flex gap-5"}>
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className={"rounded-sm p-3 hover:opacity-90"}
+            >
+              {item.name}
+            </Link>
+          ))}
+
+          <div className={"flex items-center gap-1 p-3"}>
+            <span>$1000</span>
+            <Image
+              src="/poker-chip.svg"
+              alt="Poker Chip"
+              width={28}
+              height={28}
+            />
+          </div>
+
+          <div className={"flex items-center gap-1 p-3"}>
+            <Image
+              className={"rounded-full"}
+              src="/64.png"
+              alt="User Profile"
+              width={28}
+              height={28}
+            />
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
